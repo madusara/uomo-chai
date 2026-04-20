99fe52c3be917bf90e9627181595162705af4877
Gavishka <gavishka.rightmo@gmail.com>
2026-04-21 00:55:12 +0530

    Update logo dimensions in Header9 component

--- components/headers/Header9.jsx
+++ components/headers/Header9.jsx
@@ -56,8 +56,8 @@ export default function Header9() {
           <Link href="/">
             <Image
               src="/assets/images/logo3.svg"
-              width={200}
-              height={46}
+              width={180}
+              height={42}
               alt="Uomo"
               className="logo__image d-block"
             />
